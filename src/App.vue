11765099980be64b32d9e1647cd6d89770f92091
yelee <503436375@qq.com>
2022-03-11 01:27:44 +0800

    添加图标和引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
-    你好
+    <router-view />
+    <div>
+      <i class="yelee yl-shouji"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style lang="less">
